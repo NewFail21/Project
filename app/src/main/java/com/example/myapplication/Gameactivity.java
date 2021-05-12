4848cf0ed1086d434e2099034fa3089b8ee5c67b
KVANTORIANEC <andreytim04@mail.ru>
2021-05-12 18:52:46 +0300

    Third commit

--- app/src/main/java/com/example/myapplication/Gameactivity.java
+++ app/src/main/java/com/example/myapplication/Gameactivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +12,43 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Gameactivity extends AppCompatActivity {
 
     Button buttonback1;
+    private Game game;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        buttonback1 = findViewById(R.id.back1);
         buttonback1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+        tableLayout = findViewById(R.id.tableLayout);
+        buildGameField(); // создание игрового поля
+    }
+    private TableLayout tableLayout;
+    private Button[][] buttons = new Button[3][3];
+    private void buildGameField()
+    {
+        Square[][] field = game.getField();
+        for (int i = 0, lenI = field.length; i < lenI; i++ ) {
+            TableRow row = new TableRow(this); // создание строки таблицы
+            for (int j = 0, lenJ = field[i].length; j < lenJ; j++)
+            {
+                Button button = new Button(this);
+                buttons[i][j] = button;
+
+                row.addView(button, new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                        TableRow.LayoutParams.WRAP_CONTENT)); // добавление кнопки в строку таблицы
+                button.setWidth(160);
+                button.setHeight(160);
+            }
+            tableLayout.addView(row,
+                    new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT,
+                            TableLayout.LayoutParams.WRAP_CONTENT)); // добавление строки в таблицу
+        }
     }
 }
+
---
+++ app/src/main/java/com/example/myapplication/Square.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+public class Square
+{
+    private Player player = null;
+
+    public void fill(Player player)
+    {
+        this.player = player;
+    }
+
+    public boolean isFilled()
+    {
+        return player != null;
+    }
+
+    public Player getPlayer()
+    {
+        return player;
+    }
+}
